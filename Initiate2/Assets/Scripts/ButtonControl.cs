227780b2690fd4352f1d5023f4d75ba6f3dfec2b
Davis Booth <dhb22@mpswest13.oit.duke.edu>
2019-09-19 23:04:32 -0400

    Finished a basic notepad app to get the feel for the differences between Unity and Xcode. Feel like I understand the layout of Unity a bit better now. Might be ready to try to move onto a simple AR app.

---
+++ Initiate2/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ButtonControl : MonoBehaviour {
+
+	public GameObject theText;
+
+	public void ClearText() {
+		theText.GetComponent<InputField>().text = "";
+	}
+}
